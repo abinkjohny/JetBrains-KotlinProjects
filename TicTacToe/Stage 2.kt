8c9a0859b9a7c6f0293d1bc2fec4cc65ff5ef191
Abin K Johny <83489550+abinkjohny@users.noreply.github.com>
2022-01-16 14:23:09 +0530

    Update Stage 2.kt
    Added the codes pertaining to stage 2

--- TicTacToe/Stage 2.kt
+++ TicTacToe/Stage 2.kt
@@ -1,1 +1,16 @@
+package tictactoe
 
+fun printTable(cells: String) {
+    println("---------")
+    println("| ${cells[0]} ${cells[1]} ${cells[2]} |")
+    println("| ${cells[3]} ${cells[4]} ${cells[5]} |")
+    println("| ${cells[6]} ${cells[7]} ${cells[8]} |")
+    println("---------")
+}
+
+fun main() {
+    print("Enter cells:")
+    val cells = readLine()!!
+
+    printTable(cells)
+}
