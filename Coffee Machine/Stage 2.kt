2f75e3ab8636538d7d70ec8f8621620a5a2bc74c
Abin K Johny <83489550+abinkjohny@users.noreply.github.com>
2022-01-18 11:17:58 +0530

    Update Stage 2.kt
    Added the codes necessary codes for the stage 2

--- Coffee Machine/Stage 2.kt
+++ Coffee Machine/Stage 2.kt
@@ -1,1 +1,14 @@
+package mchine
 
+import java.util.*
+
+fun main() {
+    println("Write how many cups of coffee you will need: ")
+    val scanner = Scanner(System.`in`)
+    val cups = scanner.nextInt()
+
+    println("For $cups cups of coffee you will need:")
+    println("${cups * 200} ml of water")
+    println("${cups * 50} ml of milk")
+    println("${cups * 15} g of coffee beans")
+}
